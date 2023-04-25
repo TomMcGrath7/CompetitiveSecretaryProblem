c1d272aa9f2bfaac386f0cdef7162e1c7503458d
Tom McGrath <81921465+TomMcGrath7@users.noreply.github.com>
2023-04-26 00:35:47 +0200

    Update All_Together.py

--- All_Together.py
+++ All_Together.py
@@ -56,14 +56,14 @@ def probability_candidate_chosen(n, l, alpha):
 
 
 def probability_best_chosen(n, l, k):
-    if l == 0:
-        return 1 / n
     if k == 1:
         return 1
+    if l == 0 and k != 1:
+        return 0
     elif (1 < k <= (n - l + 1)):  # confirm the right bound
         numerator = 0
         for i in range(2, k):
-            print(i)
+            # print(i)
             numerator += (1 / (i - 1))*((math.factorial(l) * math.factorial(n - l - 1) * math.factorial(n - i - 1)) /
                 (math.factorial(l - 1) * math.factorial(n - l - i)))
         denominator = math.factorial(n - 1)
@@ -81,4 +81,23 @@ alpha = (6-3)
 k = 6
 
 print(probability_best_chosen(n, l, k))
+
+""" Getting robust l """
+n = 4
+# Define array of length n-1
+probs = np.zeros(n-1)
+
+for l in range(0, n-1):
+    sum = 0
+    for k in range(1, n-l+1):
+        sum += probability_best_chosen(n, l, k)
+    probs[l] = sum
+
+print(probs)
+# find max value and index of probs
+max_value = np.amax(probs)
+print(max_value)
+# what is index of max value
+max_index = np.where(probs == max_value)
+print(max_index)
 
