251094e81e2e10076f689d9c433faa73a1522a2d
Tom McGrath <81921465+TomMcGrath7@users.noreply.github.com>
2023-05-09 20:08:21 +0200

    Update ComputeExpectedPayoffs.py

--- ComputeExpectedPayoffs.py
+++ ComputeExpectedPayoffs.py
@@ -10,44 +10,6 @@ l = 1 # int(round(n/e, 0)) # 2
 # print(l)
 print(int(round(n/e, 0)))
 
-# payoffs1 = np.empty((n, 2))
-# payoffs = np.empty((n, 2))
-#
-# for k in range(1, n+1):
-#     if (n - k - l > 0):
-#         # print(n)
-#         # print(k)
-#         # print(l)
-#         x = (math.factorial(n-1-l)*math.factorial(n-k))/(math.factorial(n-k-l)*math.factorial(n-1))
-#         # print(math.factorial(n-1-l)*math.factorial(n-k))
-#         # print(math.factorial(n-k-l)*math.factorial(n-1))
-#         # print((math.factorial(n-1-l)*math.factorial(n-k))/(math.factorial(n-k-l)*math.factorial(n-1)))
-#         payoffs1[k - 1, 0] = x
-#         payoffs1[k - 1 , 1] = (l/(n-1))
-#     else:
-#         # print(n)
-#         # print(k)
-#         # print(l)
-#         payoffs1[k - 1, 0] = 0
-#         payoffs1[k - 1, 1] = (l/(n-1))
-#
-# payoffs1[(n-l):n, 0] = 0
-
-# print(np.round(payoffs1, decimals=3))
-# print("Now including alpha")
-
-
-# print(list(range(n)))
-
-# payoffs[0:n, 0] = (1-(l/n))**(np.array(list(range(n))))
-# payoffs[0:n, 0] = (math.factorial(n-1-l)*math.factorial(n-k))/(math.factorial(n-k-l))
-# payoffs[0:n, 1] = (l/(n-1))
-# payoffs[(n-l):n, 0] = 0
-
-# print(payoffs1)
-# print(np.round(payoffs, decimals=5))
-
-
 def prob_best(x):
     return -x*np.log(x)
 
@@ -102,12 +64,6 @@ for a in range(0, n):
     index.append("k = " + current_l)
 # print(index)
 
-# columns = []
-# for a in range(0, n):
-#     current_l = str(a+1)
-#     columns.append("l = " + current_l)
-#
-# print(columns)
 
 df = pd.DataFrame(payoffs, columns=[ 'alpha = 1', 'alpha = (n-l)'], index=index )
 # print(df)
