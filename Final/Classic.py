ccd9ee2ebb74654be293c833f680e3c9673ebed1
Tom McGrath <81921465+TomMcGrath7@users.noreply.github.com>
2023-05-09 19:10:39 +0200

    Update Classic.py

--- Final/Classic.py
+++ Final/Classic.py
@@ -1,4 +1,18 @@
 import numpy as np
 
+
 def prob_best(x):
-    return -x*np.log(x)
+    return -x * np.log(x)
+
+
+""" Classic Secretary Problem Probabilities """
+
+
+def classic_probability(n, l):
+    if l == 0:
+        return 1 / n
+    else:
+        sum = 0
+        for i in range(l + 1, n + 1):
+            sum += (1 / (i - 1))
+        return (l / n) * sum
