3aa44890bbabd27401e76c974b70f61fdf8019a4
Tom McGrath <81921465+TomMcGrath7@users.noreply.github.com>
2023-05-09 19:21:54 +0200

    Update Empirical_wins.py

--- Work_in_Progress/Empirical_wins.py
+++ Work_in_Progress/Empirical_wins.py
@@ -30,6 +30,44 @@ def empirical_wins_without_choice(n, l):
 n = 10
 l = 3
 alpha = 1
+k = 3
 
 emp = empirical_wins_without_choice(n, l)
 print(emp)
+
+
+def create_custom_permutations(number_of_players, k, l, alpha):
+    candidates = np.array(list(range(1, number_of_players + 1)))
+    fixed_player = candidates[k - 1]
+    candidates = np.delete(candidates, k - 1)
+    perms = list(itertools.permutations(candidates))
+    perms = [perm[:l + alpha - 1] + (fixed_player,) + perm[l + alpha - 1:] for perm in perms]
+    return perms
+
+
+perms = create_custom_permutations(n, k, l, alpha)
+
+
+def empirical_wins(permutations, number_of_players, l):
+    wins = np.zeros(number_of_players)
+    for i in range(len(permutations)):
+        best_rank = number_of_players
+        for j in range(0, l):
+            if permutations[i][j] < best_rank:
+                best_rank = permutations[i][j]
+        best_l = best_rank
+        for k in range(l, number_of_players):
+            if permutations[i][k] < best_rank:
+                best_rank = permutations[i][k]
+                wins[best_rank - 1] += 1
+                break
+        if best_l == best_rank:
+            wins[permutations[i][number_of_players - 1] - 1] += 1
+
+    win_percents = wins / len(permutations)
+    return win_percents
+
+
+empirical_wins_with_choice = empirical_wins(perms, n, l)
+
+print(empirical_wins_with_choice)
