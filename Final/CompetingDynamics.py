ec19655d6f5c9aa5f39cef131eee306c3336a99d
Tom McGrath <t.mcgrath@figy.nl>
2023-05-22 10:45:05 +0100

    Update CompetingDynamics.py

--- Final/CompetingDynamics.py
+++ Final/CompetingDynamics.py
@@ -93,19 +93,19 @@ def best_l(n):
 
 n = 10
 cols = 2
-l = 0
+l = 9
 best_choices = np.zeros((n, cols))
 for i in range(0, n):
     k = i + 1
-    if 1 < k < (n - l + 1):
+    if 1 <= k < (n - l + 1):
         if l < (math.factorial(n - l - 1) * math.factorial(n - k)) / (
                 math.factorial(n - 2) * math.factorial(n - k - l)):
-            best_choices[i, 0] = "alpha = 1"
+            best_choices[i, 0] = 1
         elif l >= (math.factorial(n - l - 1) * math.factorial(n - k)) / (
                 math.factorial(n - 2) * math.factorial(n - k - l)):
-            best_choices[i, 0] = "alpha = (n-l)"
+            best_choices[i, 0] = (n-l)
     elif k >= (n - l + 1):
-        best_choices[i, 0] = "alpha = (n-l)"
+        best_choices[i, 0] = (n-l)
     else:
         print("We got an error")
     best_choices[i, 1] = probability_best_chosen_BR(n, l, k)
@@ -123,68 +123,94 @@ def table_it(table):
     return df
 
 
-n = 4
-probs = best_l(n)
-print("here is probbs")
-print(probs)
-print(sum(probs))
-print(probs / n)
-print(sum(probs / n))
-# max value of probs
-max_value = np.max(probs)
-print(max_value)
+" This is for printing the best responses and employer utility"
 
-print("here is the agr stuff")
-print(np.argmax(probs))
-print(np.max(probs))
-bestL = np.where(probs == max_value)
+# df = table_it(best_choices)
+# print(df)
+# print(df.to_latex(index=True))
+
+" Here will be the plot for fixed n and vary l. should be quadratic"
+" so we get an array with employer utility and l"
+
+n = 50 # This could change
 
 
-def many_n(n_max):
-    output = np.zeros((n_max, 3))
-    # best_l = np.zeros(n_max)
-    for n in range(1, n_max):
-        l_probs = best_l(n)
-        probabilities = l_probs / n
-        max_value = np.max(probabilities)
-        best_l_2 = np.argmax(probabilities)
-        output[n, 0] = n
-        output[n, 1] = best_l_2 / n
-        output[n, 2] = max_value
-
+def varied_l(n):
+    cols = 2
+    output = np.zeros((n, cols))
+    for l in range(0,n):
+        sum = 0
+        for k in range(1,n+1):
+            sum += probability_best_chosen_BR(n,l,k)
+            # Get prob. of picking best
+            # for every l get the probability of picking the best
+        output[l, 0] = l
+        output[l, 1] = sum
     return output
 
 
-output = many_n(11)
-# print(output)
-output = output[1:]
-print(output)
-
-import matplotlib.pyplot as plt
-
-# n = 10  # Length of the array
-# output = np.zeros((n, 3))  # Your 3-dimensional NumPy array
+# n = 4
+# probs = best_l(n)
+# print("here is probbs")
+# print(probs)
+# print(sum(probs))
+# print(probs / n)
+# print(sum(probs / n))
+# # max value of probs
+# max_value = np.max(probs)
+# print(max_value)
 #
-# # Generate random data for demonstration purposes
-# output[:, 0] = np.linspace(0, 1, n)  # Assign values to the first column (x-axis)
-# output[:, 1] = np.random.random(n)  # Assign random values to the second column (blue points)
-# output[:, 2] = np.random.random(n)  # Assign random values to the third column (red points)
-
-# Create the plot
-plt.scatter(output[:, 0], output[:, 1], c='blue', label='Search Fraction')
-plt.scatter(output[:, 0], output[:, 2], c='red', label='Probability Picking best')
-plt.xlabel('X-axis')
-plt.ylabel('Y-axis')
-plt.title('Plot of Points')
-plt.legend()
-plt.ylim(0, 1)  # Set the y-axis limits to 0 and 1
-
-# Display the plot
-plt.show()
-
-# folder_path = "C:\\Users\\Tom McGrath\\Desktop\\TempUni\\Master\\Thesis\\CompetitiveSecretaryProblem\\Plots"
-# filename = "plot1.png"
-# plt.savefig((folder_path + '\\' + filename))
-import math
-
-print(1 / math.e)
+# print("here is the agr stuff")
+# print(np.argmax(probs))
+# print(np.max(probs))
+# bestL = np.where(probs == max_value)
+#
+#
+# def many_n(n_max):
+#     output = np.zeros((n_max, 3))
+#     # best_l = np.zeros(n_max)
+#     for n in range(1, n_max):
+#         l_probs = best_l(n)
+#         probabilities = l_probs / n
+#         max_value = np.max(probabilities)
+#         best_l_2 = np.argmax(probabilities)
+#         output[n, 0] = n
+#         output[n, 1] = best_l_2 / n
+#         output[n, 2] = max_value
+#
+#     return output
+#
+#
+# output = many_n(11)
+# # print(output)
+# output = output[1:]
+# print(output)
+#
+# import matplotlib.pyplot as plt
+#
+# # n = 10  # Length of the array
+# # output = np.zeros((n, 3))  # Your 3-dimensional NumPy array
+# #
+# # # Generate random data for demonstration purposes
+# # output[:, 0] = np.linspace(0, 1, n)  # Assign values to the first column (x-axis)
+# # output[:, 1] = np.random.random(n)  # Assign random values to the second column (blue points)
+# # output[:, 2] = np.random.random(n)  # Assign random values to the third column (red points)
+#
+# # Create the plot
+# plt.scatter(output[:, 0], output[:, 1], c='blue', label='Search Fraction')
+# plt.scatter(output[:, 0], output[:, 2], c='red', label='Probability Picking best')
+# plt.xlabel('X-axis')
+# plt.ylabel('Y-axis')
+# plt.title('Plot of Points')
+# plt.legend()
+# plt.ylim(0, 1)  # Set the y-axis limits to 0 and 1
+#
+# # Display the plot
+# plt.show()
+#
+# # folder_path = "C:\\Users\\Tom McGrath\\Desktop\\TempUni\\Master\\Thesis\\CompetitiveSecretaryProblem\\Plots"
+# # filename = "plot1.png"
+# # plt.savefig((folder_path + '\\' + filename))
+# import math
+#
+# print(1 / math.e)
