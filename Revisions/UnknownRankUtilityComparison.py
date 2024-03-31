35ca1f231dd74f8f552b2c5f28b1d84f53a65150
Tom McGrath <81921465+TomMcGrath7@users.noreply.github.com>
2024-03-31 20:46:13 +0200

    Update UnknownRankUtilityComparison.py

--- Revisions/UnknownRankUtilityComparison.py
+++ Revisions/UnknownRankUtilityComparison.py
@@ -42,38 +42,57 @@ def sum_expression_math(n, l):
 
     return total_sum
 
-n = 2
-l = int(np.ceil(n -1/ np.e))
-print(sum_expression_np(n, l))
-print(sum_expression_math(n, l))
-print(n*l/(n-1))
+
+def c_star_n_minus_1(l, n):
+    # calculates the probability of a picking the best for a given l and n in the classic game where we can ignore
+    # the last player, c*(n-1) would be the l that maximises this expression
+    # If l is 0 then there is just a 1/n chance of picking the best. Otherwise we just do the summation from l+1 to n-1.
+    # Since we can basically ignore the last player.
+    if n == 1:
+        return 1
+    elif l == 0:
+        return 1 / (n-1)
+    else:
+        return (l / (n - 1)) * sum(1 / (i - 1) for i in range(l + 1, n))
+
+# print(sum_expression_np(n, l))
+# print(sum_expression_math(n, l))
+# print(n*l/(n-1))
 
 import matplotlib.pyplot as plt
 
-n_values = range(2, 30)
+n_values = range(2, 15)
 sum_expression_values = []
 n_l_div_n_minus_1_values = []
 
 for n in n_values:
+    best_c_star_n_minus_1_value = 0
+    current_best_l_c_star_n_minus_1 = 0
     print("n is ")
     print(n)
     print("l is ")
-    l = int(np.ceil((n -1)/ np.e))
+    for a in range(0, n):
+        # print("a is")
+        # print(a)
+        if c_star_n_minus_1(a, n) > best_c_star_n_minus_1_value:
+            best_c_star_n_minus_1_value = c_star_n_minus_1(a, n)
+            current_best_l_c_star_n_minus_1 = a
+    l = current_best_l_c_star_n_minus_1
     print(l)
     sum_expression_values.append(sum_expression_np(n, l))
     n_l_div_n_minus_1_values.append(n * l / (n - 1))
 
 plt.figure(figsize=(10, 6))
 
-# Plotting sum_expression_np(n, l) in blue
-plt.plot(n_values, sum_expression_values, color='blue', label='sum_expression_np(n, l)')
+# Plotting sum_expression_np(n, l) in blue with dots
+plt.scatter(n_values, sum_expression_values, color='blue', label='sum_expression_np(n, l)')
 
-# Plotting n*l/(n-1) in red
-plt.plot(n_values, n_l_div_n_minus_1_values, color='red', label='n*l/(n-1)')
+# Plotting n*l/(n-1) in red with dots
+plt.scatter(n_values, n_l_div_n_minus_1_values, color='red', label='n*l/(n-1)')
 
 plt.xlabel('n')
 plt.ylabel('Value')
-plt.title('Plot of the two functions')
+plt.title('Dot Plot of the two functions')
 plt.legend()
 plt.grid(True)
 plt.show()
